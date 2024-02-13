b3b8c85440253d93bbbfad857ed1d9ce19e9a9f4
Bhavishya0149 <bhavishyabhaskar1@gmail.com>
2024-02-13 17:24:33 +0530

    init

--- iampoor/lib/main.dart
+++ iampoor/lib/main.dart
@@ -2,7 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: const Text("I Am Poor", style: TextStyle(
+          color: Colors.blue
+        ),),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.blueGrey[200],
+      body: const Center(
+        child: Image(image: AssetImage("rock.png")),
+      ),
+    ),
   ));
 }
 
